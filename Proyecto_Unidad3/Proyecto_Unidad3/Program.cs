039e7bf8223e13a093679f712955dcfdf0dc185f
Maria13579 <62523764+Maria13579@users.noreply.github.com>
2020-07-08 17:48:35 -0600

    Creacion del Proyecto

---
+++ Proyecto_Unidad3/Proyecto_Unidad3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace Proyecto_Unidad3
+{
+    class Program
+    {
+        static char[,] datos = new char[3, 3];
+        static SerialPort Arduino = new SerialPort("COM5", 9600);
+        static void Main(string[] args)
+        {         
+            Arduino.Open();
+
+            if (Arduino.IsOpen)
+            {
+                Console.WriteLine("com abierto");
+                byte[] data = Encoding.ASCII.GetBytes("w");
+                Arduino.Write(data, 0, data.Length);
+                Arduino.Write("s");
+            }
+            else
+            {
+                Console.WriteLine("com cerrado");
+            }
+            Console.ReadKey();
+        }
+    }
+}
