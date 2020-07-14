b12c99e5435d000692f2e90e5a0c3fe9db323b0a
Maria13579 <62523764+Maria13579@users.noreply.github.com>
2020-07-13 19:00:35 -0600

    Update Program.cs

--- Proyecto_Unidad3/Proyecto_Unidad3/Program.cs
+++ Proyecto_Unidad3/Proyecto_Unidad3/Program.cs
@@ -31,10 +31,4 @@ namespace Proyecto_Unidad3
         }
     }
 }
-/*
-https://create.arduino.cc/editor/maria_serrano/37e5ebc5-67ed-46e2-822f-0fe948900123/preview
 
-Maria Serrano
-Mijhal Alvarez
-Karla Acevedo
-*/
